7666b3c84b35e8892cf389f86b696e7be4da0c45
Tarek S Hafez <thafez@ymail.com>
2015-09-22 12:03:02 -0500

    setting up confide and entrust with migrations, controllers, and models

--- app/models/User.php
+++ app/models/User.php
@@ -1,11 +1,12 @@
 <?php
 
+use Esensi\Model\SoftModel;
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends SoftModel implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
@@ -22,5 +23,19 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+
+	protected $rules = array(
+		'email' 	 => 'required|email|max:255|unique:users',
+		'first_name' => 'required|max:255',
+		'last_name'  => 'required|max:255',
+		'password' 	 => 'required|confirmed',
+		'address' 	 => 'required|max:255',
+		'city'    	 => 'required|max:255',
+		'state'   	 => 'required',
+		'zip'     	 => 'required|numeric'
+	);
+
+	protected $hashable = [ 'password' ];
+
 
 }
