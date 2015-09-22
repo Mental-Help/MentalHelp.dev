d5d7c999d48301150797d9fd1478cf0d71107650
meghanahrens <meghanahrens17@yahoo.com>
2015-09-22 11:12:17 -0500

    hi

---
+++ public/js/home_carousel.js
@@ -0,0 +1,6 @@
+// carousel
+$(window).bind("load resize slid.bs.carousel", function() {
+  var imageHeight = $(".active .holder").height();
+  $(".controllers").height( imageHeight );
+  console.log("Slid");
+});
