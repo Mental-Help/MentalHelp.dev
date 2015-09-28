13654aae675f67c07665e117364f54188980e288
Tarek S Hafez <thafez@ymail.com>
2015-09-28 15:17:18 -0500

    added illnessData.php modified DatabaseSeeder.php IllnessDataTableSeeder.php and IllnessesDescription.csv updated to properly format information on csv and bind their field values into the database.

--- app/database/seeds/IllnessDataTableSeeder.php
+++ app/database/seeds/IllnessDataTableSeeder.php
@@ -7,7 +7,25 @@ class IllnessDataTableSeeder extends Seeder {
 
 	public function run()
 	{
+		$handle = fopen('app/storage/illnessesDescription.csv', 'r');
+		$row = 0;
+		while($rowOfData = fgetcsv($handle)) {
+			$row++;
 
+			if ($row == 1) {
+				continue;
+			}
+
+			$illnessData = new IllnessData();
+
+			$illnessData->title 			= $rowOfData[0];
+			$illnessData->description		= $rowOfData[1];
+			$illnessData->depth_description = $rowOfData[2];
+			$illnessData->symptoms			= $rowOfData[3];
+			$illnessData->diagnosis			= $rowOfData[4];
+			$illnessData->timestamps        = false;
+			$illnessData->save();
+		}
 	}
 
 }
