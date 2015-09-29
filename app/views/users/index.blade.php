fa414bf6d40e58353648fda1222f95e6ff418ed2
meghanahrens <meghanahrens17@yahoo.com>
2015-09-29 10:22:05 -0500

    fixed the users/index page

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="container col-md-9">
+	<div class="container">
 		<h4>Search</h4>
         <div class="input-group">
             <form class="search-input navbar-form navbar-right" method="GET" role="search" action="#">
@@ -11,56 +11,58 @@
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
         </div>
+    
 
 
         <!-- Team Members Row -->
+    <div>
+        <h2 class="page-header">Scroll through our Caregivers and Patients</h2>
+    </div>
         <div class="row">
-            <div class="col-md-12">
-                <h2 class="page-header">Scroll through our Caregivers and Patients</h2>
-            </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
-            <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+            <div class="col-xs-6 col-md-3 text-center">
+                <img class="img-circle img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
                     <small>Job Title</small>
                 </h3>
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
         </div>
+
     </div>
     <!-- /.container -->
 
