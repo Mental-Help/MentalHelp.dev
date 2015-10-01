268385761a1e9e2e1ccaf1edb49405e67b3044de
Tarek S Hafez <thafez@ymail.com>
2015-10-01 15:53:30 -0500

    updated routes

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,6 @@
 Route::get('users/{users}/user', 'UsersController@role');
 Route::put('users/{users}/role', 'UsersController@editRole');
 Route::patch('users/{users}/role', 'UsersController@editRole');
-Route::get('users/index', 'UsersController@index');
 Route::resource('users', 'UsersController');
 
 //routes made for Profile CRUD Controller
@@ -24,11 +23,9 @@ Route::resource('profiles', 'ProfilesController');
 
 //routes made for User Stories CRUD Controller
 Route::resource('stories', 'UserStoriesController');
-Route::get('stories/index', 'UserStoriesController@index');
 
 //routes made for CalendarEvents CRUD Controller
 Route::get('events/attendees/{id}', 'CalendarEventsController@rsvp');
-Route::get('events/index', 'CalendarEventsController@index');
 Route::resource('events', 'CalendarEventsController');
 
 //routes made for front-end purposes
