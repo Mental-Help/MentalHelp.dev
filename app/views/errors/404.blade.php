d0f39a548d83eadff10d5c316f4f9c89301f1f00
meghanahrens <meghanahrens17@yahoo.com>
2015-10-02 14:57:29 -0500

    fixed users.index

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.master')
 @section('content')
+<div class="error">
+	<h1>Honestly. Were not sure what happened.</h1>
+	<button href="{{ action('HomeController@showHome') }}"></button>
 
+</div>
 @stop
