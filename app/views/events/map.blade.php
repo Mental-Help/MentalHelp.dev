82ab3b97e959422f38948f800cd44fbd449ae6b7
Tarek S Hafez <thafez@ymail.com>
2015-10-02 19:53:03 -0500

    removed unnecessart views, and updated mailgun api.

--- app/views/events/map.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <!-- div to hold map -->
-    <div id="map-canvas"></div>
-
-    <!-- Load the Google Maps API [DON'T FORGET TO USE A KEY] -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8ITChiDrKaqckrxmN1T7sXYekU5ZysI8"></script>
-
-@stop
