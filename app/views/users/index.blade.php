026f9189a3de6758109d7198e311fecc26c580f4
meghanahrens <meghanahrens17@yahoo.com>
2015-10-04 15:59:37 -0500

    hi

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -4,11 +4,11 @@
 
     <div class="container">
         <!-- Team Members Row -->
-        <h2 class="page-header">Scroll through our Users</h2>
+        <h2 class="page-header">All Users</h2>
 
         <div class="row">
             <div class='col-md-8'>
-                @foreach(User::all() as $user)
+                @foreach($users as $user)
                 <div class="col-xs-3 col-md-5 text-center">
                     <img class="img-circle img-center img-thumbnail" src="{{ $user->profile->image_url }}" alt="">
                     <h3>{{ $user->username }}
@@ -16,16 +16,19 @@
                     </h3>
                 </div>
                 @endforeach
+                {{ $users->links() }}
             </div>
             <div class="col-md-4">
-                <h4>Search</h4>
-                <div class="input-group">
-                    <form class="search-input navbar-form navbar-right" method="GET" role="search" action="#">
-                        <div class="form-group">
-                            <input name="search" type="text" class="form-control" placeholder="Search">
-                        </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
+                <div class="well">
+                    <h4>Search</h4>
+                    <div class="input-group">
+                        <form class="search-input navbar-form navbar-right" method="GET" role="search" action="#">
+                            <div class="form-group">
+                                <input name="search" type="text" class="form-control" placeholder="Search">
+                            </div>
+                                <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                     </div>
                  </div>
             </div>
         </div>
