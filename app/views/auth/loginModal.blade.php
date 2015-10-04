a4a8d462ac95bc41efa94453ba64c4fb73529999
Tarek S Hafez <thafez@ymail.com>
2015-10-04 14:06:12 -0500

    editing confide config to point to proper forms. LoginModal updated and master updated for functionality.

--- app/views/auth/loginModal.blade.php
+++ app/views/auth/loginModal.blade.php
@@ -7,15 +7,15 @@
             </div>
             <div class="modal-body">
                 {{ Form::open() }}
-                    <div class="form-group">
-                        {{Form::label('email', 'Email')}}
-                        {{Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email'])}}
-                    </div>
-                        
-                    <div class="form-group">
-                        {{Form::label('password', 'Password')}}
-                        {{Form::text('password', null, ['class' => 'form-control', 'placeholder' => 'Password'])}}
-                    </div>
+                <div class="form-group">
+                    {{Form::label('email', 'Email')}}
+                    {{Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email'])}}
+                </div>
+
+                <div class="form-group">
+                    {{Form::label('password', 'Password')}}
+                    {{Form::text('password', null, ['class' => 'form-control', 'placeholder' => 'Password'])}}
+                </div>
                 {{ Form::close() }}
             </div>
             <div class="modal-footer">
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -25,7 +25,36 @@
         @yield('style')
     </head>
     <body>
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="exampleModalLabel">Login</h4>
+                    </div>
+                    {{ Form::open(array('action' => 'AuthController@doLogin')) }}
+                        <div class="modal-body">
+                                <div class="form-group">
+                                    {{Form::label('email', 'Email')}}
+                                    {{Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email'])}}
+                                </div>
 
+                                <div class="form-group">
+                                    {{Form::label('password', 'Password')}}
+                                    {{Form::password('password', null, ['class' => 'form-control', 'placeholder' => 'Password'])}}
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="signup-redirect">
+                                Not a member yet?
+                                <a type="button" class="btn btn-default" data-toggle="modal" data-target="#signupModal" data-dismiss="modal" href="#">Sign Up</a>
+                            </div>
+                            {{ Form::submit('Login', ['class' => 'btn btn-default login-btn']) }}
+                        </div>
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
         @include('partials.navbar')
   
         <main>
