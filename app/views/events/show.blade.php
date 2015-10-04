2fc557baefe7ba54a5ae36b790c5063d08fbcb57
Alexandra Gutierrez <agutie95@yahoo.com>
2015-10-04 18:44:11 -0500

    input live data to events show, edited profile show

--- app/views/events/show.blade.php
+++ app/views/events/show.blade.php
@@ -1,39 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-<section>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8">
-				<h1>Support Group Title</h1>
-				<hr>
-				<p>Video chat used</p>
-				<p>date & time</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<button class="btn btn-default">RSVP</button>
+				@for($events as $event)
+					<h1>{{{$event->title}}}}</h1>
+					<hr>
+					<p>{{{$event->start_time}}} - {{{$event->end_time}}}</p>
+					<p>{{{$event->description}}}</p>
+					<button class="btn btn-default">RSVP</button>
+				@endfor
 			</div>
 
-        <div class="col-md-4">
-            <div class="well span1">
-                    <div>
-                        <img src="/img/default_profile_img.jpeg" alt="" class="img-circle img-responsive" />
-                    </div>
-                    <div>
-                        <h4>
-                            Bhaumik Patel</h4>
-                        
-                        </div>
-                    </div>
-                </div>
-            </div>
+	        <div class="col-md-4">
+		        <!-- Blog Search Well -->
+		        <div class="well">
+		            <h4>Search Events</h4>
+		            <div class="input-group">
+			            <form class="search-input navbar-form navbar-right" method="GET" role="search" action="{{ action('CalendarEventsController@index') }}">
+					    	<div class="form-group">
+					        	<input name="search" type="text" class="form-control" placeholder="Search">
+					    	</div>
+					    	<button type="submit" class="btn btn-default">Submit</button>
+					    </form>
+		            </div>
+		            <!-- /.input-group -->
+		        </div>
+		    </div>
 		</div>
-
-
-
-</section>
+	</div> <!--end container -->
 @stop
