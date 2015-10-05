9c66cddbc8f11f72e8bfbf5792dda4afb88e5861
meghanahrens <meghanahrens17@yahoo.com>
2015-10-04 23:00:31 -0500

    404 page is adoreable

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -1,9 +1,42 @@
 @extends('layouts.master')
 @section('content')
-<div class="error">
-	<div class="container">
-		<h1>Honestly. Were not sure what happened.</h1>
-		<button href="{{ action('HomeController@showHome') }}"></button>
-	</div>
-</div>
+<h1 style="color:azure;">BAYMAX- Your personal health care companion</h1>
+<h3 style="color:azure;">Honestly not sure what happened...</h3>
+<main>
+
+  <div class='head'>
+    <div class='eye'></div>
+  </div>
+
+  <div class='hands'>
+    <div class='hand'>
+      <div class='fingers'>
+        <div class='finger'></div>
+        <div class='finger'></div>
+        <div class='finger'></div>
+      </div>
+    </div>
+    <div class='hand'>
+      <div class='fingers'>
+        <div class='finger'></div>
+        <div class='finger'></div>
+        <div class='finger'></div>
+      </div>
+    </div>
+  </div>
+
+  <div class='body'>
+    <div class='logo'>
+      <div class='line'></div>
+      <div class='bord'></div>
+    </div>
+    <div class='abdo'></div>
+  </div>
+
+  <div class='lower'>
+    <div class='leg'></div>
+    <div class='leg'></div>
+  </div>
+
+</main>
 @stop
