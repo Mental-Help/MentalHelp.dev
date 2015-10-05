7dedd85973973f4df8530a34bb646bf57807aca5
Alexandra Gutierrez <agutie95@yahoo.com>
2015-10-04 19:08:06 -0500

    fixed responsiveness on user index

--- app/views/events/index.blade.php
+++ app/views/events/index.blade.php
@@ -3,20 +3,21 @@
 @section('content')
 	<div class="container">
 		<h1 class="index-heading">All Events</h1>
+        <button class="btn btn-default visitor-add-btn">Write Your Story</button>
 		<hr>
 		<div class="col-md-8">
-		@forelse ($events as $event)
-			<h3 class="index-title"><a id="index-title" class="read-more" href="{{{ action('CalendarEventsController@show', $event->id) }}}">{{{ $event->title }}}</a></h3>
-			When: {{{ $event->start_time }}}</small><br>
-			<p>{{{ Str::words($event->description, 20)}}}
-				<a class="read-more" href="{{{ action('CalendarEventsController@show', $event->id) }}}">Read More</a>
-			<p>
+			@forelse ($events as $event)
+				<h3 class="index-title"><a id="index-title" class="read-more" href="{{{ action('CalendarEventsController@show', $event->id) }}}">{{{ $event->title }}}</a></h3>
+				When: {{{ $event->start_time }}}</small><br>
+				<p>{{{ Str::words($event->description, 20)}}}
+					<a class="read-more" href="{{{ action('CalendarEventsController@show', $event->id) }}}">Read More</a>
+				<p>
 
-	    @empty
-	        <h3>There aren't any events to display at this time.</h3>
-	    @endforelse
+		    @empty
+		        <h3>There aren't any events to display at this time.</h3>
+		    @endforelse
 
-		{{ $events->appends(array('search' => Input::get('search')))->links() }}
+			{{ $events->appends(array('search' => Input::get('search')))->links() }}
 		</div>
 
 		<div class="col-md-4">
